604defae5d31cb3402220fc9ae0057179ecef773
TerZhang <terzhang@hotmail.com>
2020-06-24 05:53:35 -0400

    Updates

---
+++ precache-manifest.fbb5622c0eea5cd8b753fd70e9a56bde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2de466f9bfe66522197caf258b39987",
+    "url": "/usestatebot/index.html"
+  },
+  {
+    "revision": "ef3d1ce101f329459c6d",
+    "url": "/usestatebot/static/css/main.ad9d5147.chunk.css"
+  },
+  {
+    "revision": "bccce6dbf46a03efa5b4",
+    "url": "/usestatebot/static/js/2.185729dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/usestatebot/static/js/2.185729dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3d1ce101f329459c6d",
+    "url": "/usestatebot/static/js/main.488a622f.chunk.js"
+  },
+  {
+    "revision": "440e1fe1f895a8a547b2",
+    "url": "/usestatebot/static/js/runtime-main.81597ea5.js"
+  }
+]);
